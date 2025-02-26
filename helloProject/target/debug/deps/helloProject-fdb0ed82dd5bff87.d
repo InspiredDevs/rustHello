75b071185cdb8a8e31ff9f83f55b058a8f873ebb
Inspired Devs <inspireddevshq@gmail.com>
2025-02-26 14:48:26 +0100

    Rust Hello Intro

---
+++ helloProject/target/debug/deps/helloProject-fdb0ed82dd5bff87.d
@@ -0,0 +1,5 @@
+/media/godwin/16788F9F788F7C6D/projects/Rust/rustHello/helloProject/target/debug/deps/helloProject-fdb0ed82dd5bff87: src/main.rs
+
+/media/godwin/16788F9F788F7C6D/projects/Rust/rustHello/helloProject/target/debug/deps/helloProject-fdb0ed82dd5bff87.d: src/main.rs
+
+src/main.rs:
---
+++ helloProject/target/debug/helloProject.d
@@ -0,0 +1,1 @@
+/media/godwin/16788F9F788F7C6D/projects/Rust/rustHello/helloProject/target/debug/helloProject: /media/godwin/16788F9F788F7C6D/projects/Rust/rustHello/helloProject/src/main.rs
